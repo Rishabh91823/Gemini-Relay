ea85b42c7e89d6c85c3b3327f96c719ec755a440
Rishabh91823 <saxena.rishabh8125@gmail.com>
2025-09-15 16:00:04 +0530

    Feature: Integrate Gemini AI chatbot with @bot command

--- server.js
+++ server.js
@@ -15,20 +15,25 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+// Socket.IO setup
 const io = require('socket.io')(http);
 
 io.on('connection', (socket) => {
     console.log('A user connected...');
 
     socket.on('new-user-joined', (name) => {
+        socket.username = name;
         socket.broadcast.emit('user-joined', name);
     });
 
+    // When a message is received, broadcast the original message object to ALL clients
     socket.on('message', (msg) => {
-        socket.broadcast.emit('message', msg);
+        io.emit('message', msg);
     });
 
     socket.on('disconnect', () => {
-         console.log('A user disconnected...');
+        if (socket.username) {
+            socket.broadcast.emit('user-left', socket.username);
+        }
     });
 });
